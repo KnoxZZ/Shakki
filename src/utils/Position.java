8a43793660bc2c169a44f38717fc8d7142c4c5ff
KnoxZ_ <kallekan11@gmail.com>
2018-02-10 13:44:34 +0200

    updated Position class

--- src/utils/Position.java
+++ src/utils/Position.java
@@ -6,9 +6,25 @@ public class Position {
 	final char[] characters = {'A','B','C','D','E','F','G','H'};
 	int x;
 	int y;
+	
 	public Position(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+
+	public void stringTo(String pos) {
+		pos.replaceAll("\\s+","").toUpperCase();
+		int temp = Arrays.asList(characters).indexOf(pos.substring(1, 2));
+		int temp2 = Integer.parseInt(pos.substring(2,3));
+		if((temp > 0 && temp < 9)&&(temp2 > 0 && temp2 < 9)) {
+			this.setX(temp);
+			this.setY(temp2);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return characters[x-1] + "" + y;
 	}
 	
 	public int getX() {
@@ -25,17 +41,5 @@ public class Position {
 
 	public void setY(int y) {
 		this.y = y;
-	}
-	
-	public void stringTo(String pos) {
-		pos.replaceAll("\\s+","").toUpperCase();
-		int temp = Arrays.asList(characters).indexOf(pos.substring(1, 2));
-		this.setX(temp);
-		this.setY(Integer.parseInt(pos.substring(2,3)));
-	}
-	
-	@Override
-	public String toString() {
-		return characters[x-1] + "" + y;
 	}
 }
