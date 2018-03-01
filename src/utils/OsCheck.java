a8b7990dc3ad464f1ece10cf5147816f7ff02bce
KnoxZZ <kallekan11@gmail.com>
2018-03-01 09:58:44 +0200

    fixed the toString

--- src/utils/OsCheck.java
+++ src/utils/OsCheck.java
@@ -2,8 +2,8 @@ package utils;
 
 public class OsCheck {
 	private static String OS = null;
-	final static String[] unixCodes = {"♔","♕","♖","♗","♘","♙","♚","♛","♜","♝","♞","♟"};
-	final static String[] winCodes = {"K","Q","T","L","H","S","k","q","t","l","g","s"};
+	final static String[] unixCodes = {"♔ ","♕ ","♖ ","♗ ","♘ ","♙ ","♚ ","♛ ","♜ ","♝ ","♞ ","♟ "};
+	final static String[] winCodes = {"K ","Q ","T ","L ","H ","S ","k ","q ","t ","l ","g ","s "};
 	   public static String[] getChars()
 	   {
 	      if(OS == null) { OS = System.getProperty("os.name"); }
