fda258d783d5f41083e0c1c1d9fc4e0551e56456
KnoxZ_ <kallekan11@gmail.com>
2018-03-02 20:14:10 +0200

    revamped osCheck to clear the console

--- src/utils/OsCheck.java
+++ src/utils/OsCheck.java
@@ -1,17 +1,34 @@
 package utils;
 
+import java.io.IOException;
+
 public class OsCheck {
-	private static String OS = null;
 	final static String[] unixCodes = {"♕ ","♔ ","♖ ","♗ ","♘ ","♙ ","♛ ","♚ ","♜ ","♝ ","♞ ","♟ "};
 	final static String[] winCodes = {"K ","Q ","T ","L ","H ","S ","k ","q ","t ","l ","h ","s "};
-	   public static String[] getChars()
-	   {
-	      if(OS == null) { OS = System.getProperty("os.name"); }
-	      if(OS.equals("Linux")) {
-	    	  return unixCodes;
-	      }
-	      else {
-	    	  return winCodes;
-	      }
-	   }
+	public static String OsCheck() {
+		String OS = null;
+		if(OS == null) { OS = System.getProperty("os.name"); }
+		return OS;
+	}
+	
+	public static String[] getChars() {
+	    if(OsCheck().equals("Linux")) {
+	    	return unixCodes;
+	    }
+	    else {
+	    	return winCodes;
+	    }
+	}
+	
+	public static void clearConsole(String... errorMsg) throws InterruptedException, IOException{
+		if(OsCheck().equals("Linux")) {
+			System.out.print("\033[H\033[2J");
+		}
+		else {
+			new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			if(errorMsg != null) {
+				System.out.println(errorMsg[0]);
+			}
+		}
+	}
 }
