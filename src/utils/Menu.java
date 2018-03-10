b60658368dcc55858c4fe090500aa45e5ad8acbd
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2018-03-10 21:13:16 +0200

    Rigged save and load to menu class

--- src/utils/Menu.java
+++ src/utils/Menu.java
@@ -5,7 +5,8 @@ import java.util.*;
 
 public class Menu {
 	static Scanner r = new Scanner(System.in);
-	public void menu() throws InterruptedException, IOException {
+	
+	public static void menu(Lauta lauta) throws InterruptedException, IOException {
 		String input;
 		OsCheck.clearConsole();
 		System.out.println("1. Jatka\n2. Tallenna\n3. Lataa\n4. Lis‰‰ nappula \n5. Poista nappula");
@@ -15,8 +16,10 @@ public class Menu {
 		case 1:	
 			break;
 		case 2:
+			saveBoard(lauta);
 			break;
 		case 3:
+			Lauta pelilauta = loadBoard();
 			break;
 		case 4:
 			break;
@@ -25,7 +28,22 @@ public class Menu {
 		}
 	}
 	
-	public String getInput() {
+	public static void saveBoard(Lauta lauta) {
+		Saver saver = new Saver();
+		System.out.println("Anna tallennuksen nimi:");
+		String name = r.next();
+		saver.saveBoard(lauta, name);
+	}
+	
+	public static Lauta loadBoard() {
+		Saver saver = new Saver();
+		System.out.println("Anna tallennuksen nimi:");
+		String name = r.next();
+		Lauta lauta = saver.loadBoard(name);
+		return lauta;
+	}
+	
+	public static String getInput() {
 		String input;
 		input = r.next();
 		input.replaceAll("\\s+","");
@@ -35,13 +53,13 @@ public class Menu {
 		}
 		return input;
 	}
-	public boolean legitInput(String input) {
+	public static boolean legitInput(String input) {
 		if(input.length()==1&&isInteger(input)&&Integer.parseInt(input)<6&&Integer.parseInt(input)>0) {
 			return true;
 		}
 		return false;
 	}
-	public boolean isInteger( String input )
+	public static boolean isInteger( String input )
 	{
 	   try
 	   {
