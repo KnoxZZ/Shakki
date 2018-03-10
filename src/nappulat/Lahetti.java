f008c90daedd3d2bbe853f51263a2ba48a11008d
KnoxZ_ <kallekan11@gmail.com>
2018-03-10 21:17:12 +0200

    fixed lahetti isLegit

--- src/nappulat/Lahetti.java
+++ src/nappulat/Lahetti.java
@@ -1,6 +1,7 @@
 package nappulat;
 
-import utils.Position;
+import game.MainGame;
+import utils.*;
 
 public class Lahetti extends Nappula {
 	public Lahetti(boolean vari) {
@@ -19,8 +20,55 @@ public class Lahetti extends Nappula {
         int ex = endPos.getX();
         int ey = endPos.getY();
         if(Math.abs(sx-ex)==Math.abs(sy-ey)) {
-        	return true;
+        	if(!nappulanYli(startPos, endPos)) {
+        		return true;
+        	}
         }
+		return false;
+	}
+	
+	public boolean nappulanYli(Position startPos, Position endPos) {
+		Lauta lauta = MainGame.getLauta();
+		int sx = startPos.getX();
+        int sy = startPos.getY();
+        int ex = endPos.getX();
+        int ey = endPos.getY();
+        int px = 0;
+        int py = 0;
+        int sux = 0;
+        int suy = 0;
+
+		if(sx>ex&&sy>ey) {
+			px=ex;
+			sux=sx;
+			py=ey;
+			suy=sy;
+		}
+		else if(sx>ex&&sy<ey) {
+			px=ex;
+			sux=sx;
+			py=sy;
+			suy=ey;
+		}
+		else if(sx<ex&&sy>ey) {
+			px=sx;
+			sux=ex;
+			py=ey;
+			suy=sy;
+		}
+		else if(sx<ex&&sy<ey) {
+			px=sx;
+			sux=ex;
+			py=sy;
+			suy=ey;
+		}
+		for(int i = px; i <= sux; i++) {
+			for(int j = py; j <= suy; j++) {
+				if(lauta.getPosition(i, j).hasNappula()) {
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 	
